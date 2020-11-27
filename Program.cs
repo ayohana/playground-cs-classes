f749d65691677a324e3316f9d44d70ea89897fcc
Adela Darmansyah <adela.yohana@gmail.com>
2020-11-26 17:05:06 -0800

    Test catching exception error conditions

--- Program.cs
+++ Program.cs
@@ -8,6 +8,32 @@ namespace classes
         {
             var account = new BankAccount("Hermione Granger", 1000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with initial balance of {account.Balance}");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            // Test that initial balances must be positive.
+            try
+            {
+                BankAccount invalidAccount = new BankAccount("Ronald Weasley", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance.");
+                Console.WriteLine(e.ToString());
+            }
+
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(-750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
