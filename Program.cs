f2b7d593bbc4a815921bafc5a7a27b20e43f1a53
Adela Darmansyah <adela.yohana@gmail.com>
2020-11-27 11:36:29 -0800

    Test overdraft rules

--- Program.cs
+++ Program.cs
@@ -59,7 +59,7 @@ namespace classes
             // How much is too much to borrow?
             lineOfCredit.MakeWithdrawal(1000m, DateTime.Now, "Take out monthly advance");
             lineOfCredit.MakeDeposit(50m, DateTime.Now, "Pay back small amount");
-            //lineOfCredit.MakeWithdrawal(5000m, DateTime.Now, "Emergency funds for repairs"); //Throws an error - too much to borrow!
+            lineOfCredit.MakeWithdrawal(5000m, DateTime.Now, "Emergency funds for repairs");
             lineOfCredit.MakeDeposit(150m, DateTime.Now, "Partial restoration on repairs");
             lineOfCredit.PerformMonthEndTransactions();
             Console.WriteLine(lineOfCredit.GetAccountHistory());
